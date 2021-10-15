703c279bf6c705bfb179d655007afb9f1a949dc5
James Williams <timemagicianzero@aol.com>
2021-10-14 22:30:44 -0400

    Update Document.h
    anyways nice basic alternative science radio demo with c++ and html code... i'm using it for my new podcast coming up with guest included and my small buisness. This is just my open source code demo.

--- 2Dhologramdroneelecrgravitc&musicsysnthizer.html/Document.h
+++ 2Dhologramdroneelecrgravitc&musicsysnthizer.html/Document.h
@@ -8,5 +8,25 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface Document : NSPersistentDocument
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+#int
+reformat_programming.language:html
+
+addEventListener('load', function(e) {   document.querySelector('#test').innerHTML = 'hologram2D++cube_cpp'; });
+/*control device for electro-gravitc drone displayed on devicies such as phone and computer with out a device link besides remote link necessary....   a cube was more the mainstream thing in pop alternative science culture but i thought a basic motion sensor camera pick up camera display and touch screen was more partical and the rebuffer and adjustements features for 2D hologram display will have best output possibility to hand in all my EDx course assignments as this project being my main study and hand in assignments for all computer science courses. it'll be simple to use and not complex like my 4D rubix cube hobby which i'm talented at*/
+id<html> <javascript> x var == %shape;x,x,y,z;*val.absoulute.0 if (x).array.size.[shape.cubes.&multi.numeral.shape.value.array.(multi.formatted).multi.(color:multi.anuglar.textures]
+    _PATH_NETWORKS.connect:name.in.system.network.only.this.application.other.servers.reroute.to.@.none/no_location/*multipple shapes now coded in previous lines*/ ifelse  (y).array.size.[shape.cubes.multi.angular.textures] else revert.(x,x,y,z).array.size.[shape.cubes.multi.angular.textures]
+$.$.$.@.adresss =+(remcompiled.alternaitve.high.+;7%,8%,9%.value.-1%:-30%).fix.true.form.all.hexidecimals.array.continue.NSScriptCommand
+access.enable.this.device:camera*motion.sensor.&.touch.screen
+access.frequency.generator.enable.extnded.&linked.@wave_length==+.-
+access.microphone.only.by.create;button.icon.or.previous.lines.communication.code
+access.enable.audio:type.30%.activity.array(+,-,++,--):1,2,3,4,5,6,7,8,9,10,11@1,12@1,13@1,14@1)
+
+display.message.with.audio.DURATIONTIME.28.00567%/-0.78%:'WARNING PLANET EARTH AND IT'S SOLAR SYSTEM IS AN EXTREMELY HOSTILE ZONE DO NOT MAKE DIRECT CONTACT. THERE ARE WEAPONS FIRED AT ATLTERNATIVE SCIENCE TECHNOLGIES INTO OUTER SPACE AND WAR CRIMES ON PLANET AND INSTELLAR BUT PEACE TIME WILL SOON COME AND THE PROCESS HAS ALREADY STARTED WITH WORLD LEADERS. MY TECHNOLOGY IS PERFECTLY SAFE I JHANA10101000.'
+ </javascript> </html>
+enable.device.access.with.permissions.video.libary.&.picture.libary.display
+display.enable.application.name'garageband'.item.'piano'
+                
+display.data.feed
 @end
